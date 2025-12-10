178700cc422a39569b539b83481eaad37250ba41
Zaro <elliotraystephenson@gmail.com>
2025-12-10 11:29:42 -0500

    Update sheep.js

--- sheep.js
+++ sheep.js
@@ -1,61 +1,182 @@
-{
-    const sheepOne = document.getElementById("s1");
-    var isDragging = false;
+const sheepA = document.getElementById("sheep");
+let penA = document.getElementById("pen1");
+let penB = document.getElementById("pen2");
+let penC = document.getElementById("pen3");
 
-    sheepOne.addEventListener('mousedown', (event) => {
-        console.log('Sheep clicked');
-        var isDragging = true;
+const pen0 = "Pen.png";
+const pen1 = "Pen1.png";
+const pen2 = "Pen2.png";
+const pen3 = "Pen3.png";
+const pen4 = "Pen4.png";
+const pen5 = "Pen5.png";
+const pen6 = "Pen6.png";
+const pen7 = "Pen7.png";
+const pen8 = "Pen8.png";
+const pen9 = "Pen9.png";
 
-        var cxpos = Event.clientX;
-        var cypos = Event.clientY;
+let whatever = "whatever";
 
-        var sonerect = sheepOne.getBoundingClientRect();
 
-        var osxpos = sonerect.x;
-        var osypos = sonerect.y;
-    });
+let clickCountA = 0;
+let clickCountB = 0;
+let clickCountC = 0;
 
-    async function move(event) {
-
-        await delay(500);
-
-        var cxpos = Event.clientX;
-        var cypos = Event.clientY;
-
-        var sonerect = sheepOne.getBoundingClientRect();
-
-        var sxpos = sonerect.x;
-        var sypos = sonerect.y;
-
-        sheepOne.style.left = sxpos + (cxpos-sxpos);
-        sheepOne.style.top = sypos + (cypos-sxpos);
-        console.log('moving mouse!');
-    };
-
-    sheepOne.addEventListener('mousemove', (move));
-
-    function dropped(event) {
-        var cxpos = Event.clientX;
-        var cypos = Event.clientY;
-
-        var newsxpos = osxpos + (cxpos-osxpos);
-        var newsypos = osypos + (cxpos-osxpos);
-
-        sheepOne.style.left = `${newsxpos}px`;
-        sheepOne.style.top = `${newsypos}px`;
-    };
-
-    sheepOne.addEventListener('mouseup', (dropped));
-
+penA.addEventListener("click", function() {
+    clickCountA = clickCountA + 1;
+    
+if (`pen${clickCountA}`=="pen1") {
+    penA.src = pen1;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen2") {
+    penA.src = pen2;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen3") {
+    penA.src = pen3;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen4") {
+    penA.src = pen4;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen5") {
+    penA.src = pen5;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen6") {
+    penA.src = pen6;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen7") {
+    penA.src = pen7;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen8") {
+    penA.src = pen8;
+    return;
+} else {
+if (`pen${clickCountA}`=="pen9") {
+    penA.src = pen9;
+    return;
+ } else {
+if (`pen${clickCountA}`=="pen0") {
+    penA.src = pen0;
+    return;
 }
+} } } } } } } } }
 
-{
-    var sheepOne = document.getElementById("s1");
+`pen${clickCountA}`;
+    whatever = penA.src;
 
-    sheepOne.addEventListener('dragstop', (event) => {
+    console.log(clickCountA);
+    console.log(whatever);
+});
 
-        let cloneSheepOne = sheepOne.cloneNode(false);
 
-        cloneSheepOne.style.top = eval("sheepOne - eventClient.Y")
-    })
+
+penB.addEventListener("click", function() {
+    clickCountB = clickCountB + 1;
+    
+if (`pen${clickCountB}`=="pen1") {
+    penB.src = pen1;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen2") {
+    penB.src = pen2;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen3") {
+    penB.src = pen3;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen4") {
+    penB.src = pen4;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen5") {
+    penB.src = pen5;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen6") {
+    penB.src = pen6;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen7") {
+    penB.src = pen7;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen8") {
+    penB.src = pen8;
+    return;
+} else {
+if (`pen${clickCountB}`=="pen9") {
+    penB.src = pen9;
+    return;
+ } else {
+if (`pen${clickCountB}`=="pen0") {
+    penB.src = pen0;
+    return;
 }
+} } } } } } } } }
+
+`pen${clickCountB}`;
+    whatever = penB.src;
+
+    console.log(clickCountB);
+    console.log(whatever);
+});
+
+
+
+penC.addEventListener("click", function() {
+    clickCountC = clickCountC + 1;
+    
+if (`pen${clickCountC}`=="pen1") {
+    penC.src = pen1;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen2") {
+    penC.src = pen2;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen3") {
+    penC.src = pen3;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen4") {
+    penC.src = pen4;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen5") {
+    penC.src = pen5;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen6") {
+    penC.src = pen6;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen7") {
+    penC.src = pen7;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen8") {
+    penC.src = pen8;
+    return;
+} else {
+if (`pen${clickCountC}`=="pen9") {
+    penC.src = pen9;
+    return;
+ } else {
+if (`pen${clickCountC}`=="pen0") {
+    penC.src = pen0;
+    return;
+}
+} } } } } } } } }
+
+`pen${clickCountC}`;
+    whatever = penC.src;
+
+    console.log(clickCountC);
+    console.log(whatever);
+});
